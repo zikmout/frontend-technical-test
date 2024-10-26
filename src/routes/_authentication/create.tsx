93956570dd97101da244c79105ed59b3f82bd9cc
Simon Sicard <sicsim64@gmail.com>
2024-10-26 12:20:32 +0200

    wip: fix meme upload form

--- src/routes/_authentication/create.tsx
+++ src/routes/_authentication/create.tsx
@@ -10,11 +10,13 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
+import { useAuthToken } from "../../contexts/authentication";
 import { MemeEditor } from "../../components/meme-editor";
 import { useMemo, useState } from "react";
 import { MemePictureProps } from "../../components/meme-picture";
 import { Plus, Trash } from "@phosphor-icons/react";
+import axios from "axios";
 
 export const Route = createFileRoute("/_authentication/create")({
   component: CreateMemePage,
@@ -26,8 +28,12 @@ type Picture = {
 };
 
 function CreateMemePage() {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+  const token = useAuthToken();
   const [picture, setPicture] = useState<Picture | null>(null);
   const [texts, setTexts] = useState<MemePictureProps["texts"]>([]);
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
 
   const handleDrop = (file: File) => {
     setPicture({
@@ -51,16 +57,50 @@ function CreateMemePage() {
     setTexts(texts.filter((_, i) => i !== index));
   };
 
+  const handleDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(e.target.value);
+  };
+
   const memePicture = useMemo(() => {
     if (!picture) {
       return undefined;
     }
-
     return {
       pictureUrl: picture.url,
       texts,
     };
   }, [picture, texts]);
+
+  const handleSubmit = async () => {
+    if (!picture || !description || texts.length === 0) return;
+
+    const formData = new FormData();
+    formData.append("Picture", picture.file);
+
+    formData.append("Description", description.trim());
+
+    texts.forEach((text, index) => {
+      const roundedX = Math.round(text.x);
+      const roundedY = Math.round(text.y);
+      formData.append(`Texts[${index}][Content]`, text.content);
+      formData.append(`Texts[${index}][X]`, roundedX.toString());
+      formData.append(`Texts[${index}][Y]`, roundedY.toString());
+    });
+
+    try {
+      await axios.post(`${baseUrl}/memes`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      navigate({ to: "/" });
+    } catch (error) {
+      console.error("Échec de la soumission du meme :", error);
+    }
+  };
 
   return (
     <Flex width="full" height="full">
@@ -76,7 +116,11 @@ function CreateMemePage() {
             <Heading as="h2" size="md" mb={2}>
               Describe your meme
             </Heading>
-            <Textarea placeholder="Type your description here..." />
+            <Textarea
+              placeholder="Type your description here..."
+              value={description}
+              onChange={handleDescriptionChange}
+            />
           </Box>
         </VStack>
       </Box>
@@ -93,8 +137,16 @@ function CreateMemePage() {
         <Box p={4} flexGrow={1} height={0} overflowY="auto">
           <VStack>
             {texts.map((text, index) => (
-              <Flex width="full">
-                <Input key={index} value={text.content} mr={1} />
+              <Flex width="full" key={index}>
+                <Input
+                  value={text.content}
+                  onChange={(e) => {
+                    const newTexts = [...texts];
+                    newTexts[index].content = e.target.value;
+                    setTexts(newTexts);
+                  }}
+                  mr={1}
+                />
                 <IconButton
                   onClick={() => handleDeleteCaptionButtonClick(index)}
                   aria-label="Delete caption"
@@ -131,7 +183,8 @@ function CreateMemePage() {
             size="sm"
             width="full"
             color="white"
-            isDisabled={memePicture === undefined}
+            onClick={handleSubmit}
+            isDisabled={!picture || description === ""}
           >
             Submit
           </Button>
