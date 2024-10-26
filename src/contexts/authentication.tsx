b4158e02f02290d363f372fe34785adb0955d4ee
Simon Sicard <sicsim64@gmail.com>
2024-10-26 13:19:49 +0200

    persist authentication on localStorage

--- src/contexts/authentication.tsx
+++ src/contexts/authentication.tsx
@@ -6,7 +6,9 @@ import {
   useContext,
   useMemo,
   useState,
+  useEffect,
 } from "react";
+import axios from "axios";
 
 export type AuthenticationState =
   | {
@@ -25,35 +27,63 @@ export type Authentication = {
 };
 
 export const AuthenticationContext = createContext<Authentication | undefined>(
-  undefined,
+  undefined
 );
 
 export const AuthenticationProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
-  const [state, setState] = useState<AuthenticationState>({
-    isAuthenticated: false,
+  const [state, setState] = useState<AuthenticationState>(() => {
+    // Use local storage to persist the authentication state
+    const savedToken = localStorage.getItem("authToken");
+    if (savedToken) {
+      const userId = jwtDecode<{ id: string }>(savedToken).id;
+      return {
+        isAuthenticated: true,
+        token: savedToken,
+        userId,
+      };
+    }
+    return { isAuthenticated: false };
   });
 
   const authenticate = useCallback(
     (token: string) => {
+      localStorage.setItem("authToken", token);
       setState({
         isAuthenticated: true,
         token,
         userId: jwtDecode<{ id: string }>(token).id,
       });
     },
-    [setState],
+    [setState]
   );
 
   const signout = useCallback(() => {
+    localStorage.removeItem("authToken");
     setState({ isAuthenticated: false });
   }, [setState]);
 
   const contextValue = useMemo(
     () => ({ state, authenticate, signout }),
-    [state, authenticate, signout],
+    [state, authenticate, signout]
   );
+
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          signout();
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, [signout]);
 
   return (
     <AuthenticationContext.Provider value={contextValue}>
